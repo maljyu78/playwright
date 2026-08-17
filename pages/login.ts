a9601de614b98db020f0df7aad29216bcec76eec
jinyong <maljyu78@gmail.com>
2026-08-17 18:00:19 +0900

    Add locators, actions to login.ts

--- pages/login.ts
+++ pages/login.ts
@@ -1,4 +1,42 @@
-import { Page, Locator } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 
-//s
+export class LoginPage {
+  readonly page: Page;
+  // Locators
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
 
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  // Actions
+
+  async goto() {
+    await this.page.goto('https://www.saucdemo.com/');
+  }
+  async enterUsername(username: string) {
+    await this.usernameInput.fill(username);
+  }
+  async enterPassword(password: string) {
+    await this.passwordInput.fill(password);
+  }
+  async clickLogin() {
+    await this.loginButton.click();
+  }
+  async login(username: string, password: string) {
+    await this.enterUsername(username);
+    await this.enterPassword(password);
+    await this.clickLogin();
+  }
+
+  async getErrorMessage(message: string) {
+    await expect(this.errorMessage).toContainText(message);
+  }
+}
