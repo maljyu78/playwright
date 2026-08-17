2268fc20c9b000200440a1ea3b5a1b5ad029efd0
jinyong <maljyu78@gmail.com>
2026-08-17 18:18:34 +0900

    Add test-data.ts

---
+++ data/test-data.ts
@@ -0,0 +1,17 @@
+export const loginData = {
+  standardUser: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+
+  wrong_PW: {
+    username: 'standard_user',
+    password: 'wrong_password',
+  },
+};
+
+export const productData = {
+  backpack: 'Sauce Labs Backpack',
+  bikeLight: 'Sauce Labs Bike Light',
+  boltTShirt: 'Sauce Labs Bolt T-Shirt',
+};
